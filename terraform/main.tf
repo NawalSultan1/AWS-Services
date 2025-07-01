9083c7b60f0c5702254e6c914d88cb800c79f99e
NawalSultan1 <nawalsultan2001@gmail.com>
2025-07-02 04:23:02 +0500

    devops practices

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,5 +5,5 @@ resource "aws_instance" "first_Instance" {
   ami = "ami-020cba7c55df1f615"   //for using ubuntu server
   instance_type = "t2.micro"
   subnet_id = "your-subnet-id"
-  key_name = "your-keypair-name"
+  key_name = "your-keypair"
 }
