8ecfa20a96e6ce4fd2de5a8cecd2b887c824eb57
NawalSultan1 <nawalsultan2001@gmail.com>
2025-07-02 04:17:28 +0500

    IaC to create simple ec2 instance on aws

---
+++ terraform/main.tf
@@ -0,0 +1,9 @@
+provider "aws" {
+  region = "us-east-1" //region you want to init the server on
+}
+resource "aws_instance" "first_Instance" {
+  ami = "ami-020cba7c55df1f615"   //for using ubuntu server
+  instance_type = "t2.micro"
+  subnet_id = "your-subnet-id"
+  key_name = "your-keypair-name"
+}
